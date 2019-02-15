0681595399a05e75df33ddf6e8ada1b24720db2e
Anna Xambó <sketch.foo@gmail.com>
2019-02-15 08:57:19 +0100

    changes d7

--- code/d7/02-touch-events/app2.js
+++ code/d7/02-touch-events/app2.js
@@ -15,18 +15,25 @@ window.onload = function() {
     //   oscillator.start();
     // });
  
-    document.querySelector("#button1").addEventListener('touchstart', touchStart, false);
-    document.querySelector("#button1").addEventListener('click', touchStart, false);
+    document.addEventListener('touchstart', touchStart, false);
+    document.addEventListener('click', touchStart, false);
     
     function touchStart(event) {
       if(playing) return;
+      event.preventDefault();
       playing = true;  
       context = new AudioContext();
       oscillator = context.createOscillator();
       oscillator.connect(context.destination);
       oscillator.frequency.value = 440;
       oscillator.type = "triangle"; // "sine", "square", "sawtooth", "triangle"
-      oscillator.start();      
+      oscillator.start(0);
+      // if (oscillator.start) {
+      //   oscillator.start(0);
+      // }
+      // else {
+      //     oscillator.noteOn(0);
+      // }    
     } 
 
     document.querySelector("#button2").addEventListener('touchstart', touchStop, false);
@@ -34,6 +41,7 @@ window.onload = function() {
 
     function touchStop(event) {
       playing = false;  
+      event.preventDefault();
       oscillator.stop();      
     } 
 
